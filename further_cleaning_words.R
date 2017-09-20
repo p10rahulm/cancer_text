39e0ef1ae49acb6cf2e71052f8972cc73a3c30e4
Rahul Madhavan <rahul.maddy@gmail.com>
2017-09-20 22:49:38 +0530

    made some changes in R

--- further_cleaning_words.R
+++ further_cleaning_words.R
@@ -1,5 +1,5 @@
 # -------------
-# Clean after removing words
+# Clean after removing words - training
 # -------------
 rm(list=ls())
 load("cleaned/training_words_removed.bin")
@@ -11,6 +11,7 @@ synopses <- gsub("\\([0-9]+\\, [0-9]+\\)"," ",synopses)
 synopses <- gsub("\\([0-9]+\\, [0-9]+\\, [0-9]+\\)"," ",synopses)
 synopses <- gsub("\\([0-9]+\\, [0-9]+\\, [0-9]+\\, [0-9]+\\)"," ",synopses)
 synopses <- gsub("[[:punct:]]", "", synopses)
+synopses <- gsub(" [0-9] "," ",synopses)
 synopses <- gsub(" [0-9][0-9] "," ",synopses)
 synopses <- gsub(pattern = " +"," ", synopses)
 synopses <- gsub(pattern = "^ ","", synopses)
@@ -19,3 +20,27 @@ training_variants$Synopsis <- synopses
 save(training_variants,file = "cleaned/training_words_removed_cleaned.bin")
 
 write(x = synopses,file = "cleaned/training_synopses.txt")
+
+
+# -------------
+# Clean after removing words - testing
+# -------------
+rm(list=ls())
+load("cleaned/testing_words_removed.bin")
+synopses <- testing_variants$Synopsis
+synopses <- iconv(synopses, "latin1", "ASCII", sub="")
+synopses <- gsub("[[0-9]+]"," ",synopses)
+synopses <- gsub("\\([0-9]+\\)"," ",synopses)
+synopses <- gsub("\\([0-9]+\\, [0-9]+\\)"," ",synopses)
+synopses <- gsub("\\([0-9]+\\, [0-9]+\\, [0-9]+\\)"," ",synopses)
+synopses <- gsub("\\([0-9]+\\, [0-9]+\\, [0-9]+\\, [0-9]+\\)"," ",synopses)
+synopses <- gsub("[[:punct:]]", "", synopses)
+synopses <- gsub(" [0-9] "," ",synopses)
+synopses <- gsub(" [0-9][0-9] "," ",synopses)
+synopses <- gsub(pattern = " +"," ", synopses)
+synopses <- gsub(pattern = "^ ","", synopses)
+synopses <- gsub(pattern = " $","", synopses)
+testing_variants$Synopsis <- synopses
+save(testing_variants,file = "cleaned/testing_words_removed_cleaned.bin")
+
+write(x = synopses,file = "cleaned/testing_synopses.txt")
--- removewords.R
+++ removewords.R
@@ -3,11 +3,6 @@ rm(list=ls())
 # -------------
 # Load Libraries
 # -------------
-library(stringr)
-library(tm)
-# install.packages("RWeka")
-library(RWeka)
-library(SnowballC)
 
 # -------------
 # readfiles
@@ -19,21 +14,16 @@ load("cleaned/training_variants.bin")
 # Read English Dictionaries
 # -------------
 
-# All english words
-engwords <- readLines("rawdata/eng_dictionary.txt")
-engwords <-tolower(engwords)
+
 # Norvig by frequency
 word_freq <- read.table("rawdata/word_frequencies.txt")
 word_freq <- word_freq[1:30000,1]
 word_freq <- as.character(word_freq)
 word_freq <-tolower(word_freq)
-# word_freq <- gsub(pattern = "^",replacement = " ",x = word_freq,perl = T)
-# word_freq <- gsub(pattern = "$",replacement = " ",x = word_freq,perl = T)
 write(word_freq,file="rawdata/words_to_remove.csv")
-word_freq_subset <- word_freq[1:100]
-write(word_freq_subset,file="rawdata/words_to_remove_subset.csv")
+
 # -------------
-# Remove EnglishWords from text
+# Remove EnglishWords from text - training
 # -------------
 synopses <- as.character(training_variants$Synopsis)
 synopses <-tolower(synopses)
@@ -50,16 +40,38 @@ save(training_variants,file = "cleaned/training_words_removed.bin")
 rm(i,a,synopses)
 
 # -------------
-# Remove All EnglishWords from text
+# Remove EnglishWords from text - testing
 # -------------
-synopses <- as.character(training_variants$Synopsis)
+synopses <- as.character(testing_variants$Synopsis)
 synopses <-tolower(synopses)
 
 i=0
-for(i in 0:465){
-  a <- paste0("\\b(",paste(engwords[(i*1000+1):(i*1000+1000)],collapse = "|"),")\\b")
+for(i in 0:14){
+  a <- paste0("\\b(",paste(word_freq[(i*2000+1):(i*2000+2000)],collapse = "|"),")\\b")
   synopses <- gsub(pattern = a,replacement = " ",x = synopses,perl = T)
   print(i)
 }
-training_variants$Synopsis <- synopses
-save(training_variants,file = "cleaned/training_all_words_removed.bin")
+testing_variants$Synopsis <- synopses
+save(testing_variants,file = "cleaned/testing_words_removed.bin")
+
+rm(i,a,synopses)
+
+# -------------
+# Remove All EnglishWords from text
+# -------------
+# All english words
+# Below seems excessive removal. Not going ahead at present
+# engwords <- readLines("rawdata/eng_dictionary.txt")
+# engwords <-tolower(engwords)
+# 
+# synopses <- as.character(training_variants$Synopsis)
+# synopses <-tolower(synopses)
+# 
+# i=0
+# for(i in 0:465){
+#   a <- paste0("\\b(",paste(engwords[(i*1000+1):(i*1000+1000)],collapse = "|"),")\\b")
+#   synopses <- gsub(pattern = a,replacement = " ",x = synopses,perl = T)
+#   print(i)
+# }
+# training_variants$Synopsis <- synopses
+# save(training_variants,file = "cleaned/training_all_words_removed.bin")
