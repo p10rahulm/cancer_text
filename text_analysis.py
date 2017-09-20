cc50d766197f5bc5f77c25f4339800f3181f6c2b
Rahul Madhavan <rahul.maddy@gmail.com>
2017-09-20 22:29:54 +0530

    added np.save into binary

--- text_analysis.py
+++ text_analysis.py
@@ -20,25 +20,17 @@ for synopses_line in synopses_text:
 
 sorted_dictionary_tuplelist = sorted(full_dict.items(), key=operator.itemgetter(1),  reverse=True)
 
-# sorted_word_frequency_file = open('cleaned/sorted_word_frequency.txt', 'w')
-# for item in sorted_dictionary_tuplelist:
-#     sorted_word_frequency_file.write("%s\n" % item)
-# print(sorted_dictionary_tuplelist)
 sorted_dictionary_list = [itemtuple[0] for itemtuple in sorted_dictionary_tuplelist]
 sorted_dictionary_index = {sorted_dictionary_list[i]:i for i in range(0,len(sorted_dictionary_list))}
 
-# print(len(synopses_text))
-# print(sorted_dictionary_list[-1])
-# print(sorted_dictionary_index[sorted_dictionary_list[-1]])
 
-inputmatrix = np.zeros(shape=(len(synopses_text),len(sorted_dictionary_list)))
-print(synopses_text[0])
+outputmatrix = np.zeros(shape=(len(synopses_text),len(sorted_dictionary_list)), dtype=np.int8)
+
 for synopses_line_no in range(0,len(synopses_text)):
     words = synopses_text[synopses_line_no].split()
     for word in words:
-        inputmatrix[synopses_line_no,(sorted_dictionary_index[word]-1)] += 1
-        # if(synopses_line_no==1):
-        #     print(word)
+        outputmatrix[synopses_line_no,(sorted_dictionary_index[word]-1)] += 1
 
-# print(inputmatrix[0:20,])
+print(outputmatrix [0:20,])
+np.save("cleaned/outputtrainmatrix", outputmatrix )
 
