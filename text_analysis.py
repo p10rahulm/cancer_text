c58ecd042ca24876501365a66344e9595eec58b7
Rahul Madhavan <rahul.maddy@gmail.com>
2017-10-07 14:52:34 +0530

    changes in functionality

--- text_analysis.py
+++ text_analysis.py
@@ -53,7 +53,7 @@ wordcount_doc = np.zeros(shape=(len(synopses_text), len(sorted_dictionary_list))
 for synopses_line_no in range(0, len(synopses_text)):
     words = synopses_text[synopses_line_no].split()
     for word in words:
-        wordcount_doc[synopses_line_no, (sorted_dictionary_index[word] - 1)] += 1
+        wordcount_doc[synopses_line_no, (sorted_dictionary_index[word])] += 1
 
 
 # --------------
@@ -75,10 +75,40 @@ for i in range(1, 10):
 # Row-wise sums
 # --------------
 numdocs_word = np.zeros(shape=(9, len(sorted_dictionary_list)), dtype=np.int16)
-num_occurances_indoc_word = np.zeros(shape=(9, len(sorted_dictionary_list)), dtype=np.int16)
+num_occurances_indoc_word = np.zeros(shape=(9, len(sorted_dictionary_list)), dtype=np.int32)
+prob_occurances_indoc_word = np.zeros(shape=(9, len(sorted_dictionary_list)), dtype=np.float)
 for i in range(0, 9):
     numdocs_word[i,] = np.sum(exist_cat_list[i], axis=0)
     num_occurances_indoc_word[i,] = np.sum(count_cat_list[i], axis=0)
+
+# Get probabilities of word counts per page
+prob_occurances_indoc_word = np.zeros(shape=(9, len(sorted_dictionary_list)), dtype=np.float)
+prob_occurances_indoc_word = num_occurances_indoc_word/np.sum(num_occurances_indoc_word,axis=0)
+
+
+# We are going to find out weights for each word. This weight is going to be based on two factors.
+
+# Constants
+alpha = 0.5
+beta = 25
+transform_power = 2.5
+numwords = np.ma.count(prob_occurances_indoc_word,axis=1)[0]
+mean = np.sum(prob_occurances_indoc_word,axis=1)/numwords
+mean_tr = (mean*9)**transform_power
+
+prob_occurances_indoc_word_tr = (prob_occurances_indoc_word*9)**transform_power
+mean_shaped = np.reshape(np.repeat(mean_tr,numwords),(9,numwords))
+part_a_wt = np.sum((prob_occurances_indoc_word_tr-mean_shaped)**2,axis=0)**alpha
+part_b_wt = np.log(np.sum((num_occurances_indoc_word)**2,axis=0))/np.log(beta)
+print(np.max(part_a_wt))
+print(np.max(part_b_wt))
+wts_arr = part_a_wt*part_b_wt
+print("part_a_wt")
+print(part_a_wt[1:10])
+print(part_a_wt[160:165])
+print("wts_array")
+print(wts_arr[1:10])
+print(wts_arr[160:165])
 
 # IDHL indicates category 8 or 9
 print("numdocs_word")
